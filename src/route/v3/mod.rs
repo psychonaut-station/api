d1606bd81a761155f79e8300b9c70e0bdc48f390
Sefa <50076109+Seefaaa@users.noreply.github.com>
2025-08-11 20:30:51 +0300

    refactor: simplify service macro

--- src/route/v3/mod.rs
+++ src/route/v3/mod.rs
@@ -2,18 +2,12 @@ mod player;
 mod recent_test_merges;
 mod server;
 
-use poem_openapi::OpenApiService;
-
 macro_rules! service {
-    ($($endpoint:path),*) => {
-        pub fn service() -> OpenApiService<($($endpoint,)*), ()> {
-            OpenApiService::new(($($endpoint,)*), "Psychonaut Station API", "3.0.0").url_prefix("/v3")
+    ($($endpoint:ident),*) => {
+        pub fn service() -> poem_openapi::OpenApiService<($($endpoint::Endpoint,)*), ()> {
+            poem_openapi::OpenApiService::new(($($endpoint::Endpoint,)*), "Psychonaut Station API", "3.0.0").url_prefix("/v3")
         }
     };
 }
 
-service!(
-    server::Endpoint,
-    player::Endpoint,
-    recent_test_merges::Endpoint
-);
+service!(server, player, recent_test_merges);
