2b63480e3e461cb156bed974fe3be10f7feb412d
Sefa <50076109+Seefaaa@users.noreply.github.com>
2025-08-12 21:35:22 +0300

    refactor: generate html by ourself

--- src/route/mod.rs
+++ src/route/mod.rs
@@ -1,10 +1,14 @@
 mod v3;
 
-use poem::Route;
+use poem::{Route, endpoint::make_sync, web::Html};
+
+const STOPLIGHT_ELEMENTS: &str = include_str!("stoplight-elements.html");
 
 pub(super) fn route() -> Route {
     let service = v3::service();
-    let ui = service.stoplight_elements();
+    let ui_html = STOPLIGHT_ELEMENTS.replace("'{:spec}'", &service.spec());
 
-    Route::new().nest("/v3", service).nest("/", ui)
+    Route::new()
+        .nest("/v3", service)
+        .nest("/", make_sync(move |_| Html(ui_html.clone())))
 }
