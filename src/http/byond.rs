8b2c767f4d47c095cfba912040ec8c8025be8ff2
Sefa <50076109+Seefaaa@users.noreply.github.com>
2025-08-13 20:10:41 +0300

    selenium

--- src/http/byond.rs
+++ src/http/byond.rs
@@ -1,18 +1,35 @@
-use super::{Error, REQWEST_CLIENT};
+use tokio::process::Command;
+
+use super::Error;
 
 pub async fn is_member(ckey: &str) -> Result<bool, Error> {
-    let response = REQWEST_CLIENT
-        .get(format!(
-            "https://secure.byond.com/members/{ckey}?format=text"
-        ))
-        .send()
-        .await?;
+    // let response = REQWEST_CLIENT
+    //     .get(format!(
+    //         "https://secure.byond.com/members/{ckey}?format=text"
+    //     ))
+    //     .send()
+    //     .await?;
 
-    if let Some(content_type) = response.headers().get("content-type") {
-        if let Ok(content_type) = content_type.to_str() {
-            return Ok(content_type.split(';').next() == Some("text/plain"));
+    // if let Some(content_type) = response.headers().get("content-type") {
+    //     if let Ok(content_type) = content_type.to_str() {
+    //         return Ok(content_type.split(';').next() == Some("text/plain"));
+    //     }
+    // }
+
+    let output = Command::new("/member.py").arg(ckey).output().await?;
+
+    if output.status.success() {
+        let stdout = String::from_utf8_lossy(&output.stdout);
+        let stdout = stdout.trim();
+
+        if stdout == "true" {
+            return Ok(true);
+        } else if stdout == "false" {
+            return Ok(false);
+        } else {
+            return Err(Error::MemberCheckFailed(ckey.to_string()));
         }
     }
 
-    Ok(false)
+    Err(Error::MemberCheckFailed(ckey.to_string()))
 }
--- src/http/error.rs
+++ src/http/error.rs
@@ -5,6 +5,9 @@ use thiserror::Error;
 pub enum Error {
     Reqwest(#[from] reqwest::Error),
     SerdeJson(#[from] serde_json::Error),
+    Io(#[from] std::io::Error),
     #[error("discord api error")]
     Discord(u32),
+    #[error("member check failed for ckey: {0}")]
+    MemberCheckFailed(String),
 }
