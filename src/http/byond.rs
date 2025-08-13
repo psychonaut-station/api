8e3ab6049f81243f00f3c5b96af7ead8e149dc98
Sefa <50076109+Seefaaa@users.noreply.github.com>
2025-08-13 21:01:34 +0300

    aha

--- src/http/byond.rs
+++ src/http/byond.rs
@@ -1,35 +1,20 @@
-use tokio::process::Command;
+use crate::http::REQWEST_CLIENT;
 
 use super::Error;
 
 pub async fn is_member(ckey: &str) -> Result<bool, Error> {
-    // let response = REQWEST_CLIENT
-    //     .get(format!(
-    //         "https://secure.byond.com/members/{ckey}?format=text"
-    //     ))
-    //     .send()
-    //     .await?;
+    let response = REQWEST_CLIENT
+        .get(format!(
+            "http://selenium-proxy:8000/?url=https://secure.byond.com/members/{ckey}?format=text"
+        ))
+        .send()
+        .await?;
 
-    // if let Some(content_type) = response.headers().get("content-type") {
-    //     if let Ok(content_type) = content_type.to_str() {
-    //         return Ok(content_type.split(';').next() == Some("text/plain"));
-    //     }
-    // }
-
-    let output = Command::new("/member.py").arg(ckey).output().await?;
-
-    if output.status.success() {
-        let stdout = String::from_utf8_lossy(&output.stdout);
-        let stdout = stdout.trim();
-
-        if stdout == "true" {
-            return Ok(true);
-        } else if stdout == "false" {
-            return Ok(false);
-        } else {
-            return Err(Error::MemberCheckFailed(ckey.to_string()));
+    if let Some(content_length) = response.headers().get("content-length") {
+        if let Ok(Ok(content_length)) = content_length.to_str().map(|s| s.parse::<u32>()) {
+            return Ok(content_length < 1000);
         }
     }
 
-    Err(Error::MemberCheckFailed(ckey.to_string()))
+    Ok(false)
 }
